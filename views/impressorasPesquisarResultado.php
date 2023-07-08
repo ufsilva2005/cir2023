f4c2ffecd2737ce3615c2b18e2af97e1521435a7
Ulisses <ufsilva2005@gmail.com>
2023-07-08 12:56:58 -0300

    arterCasa

--- views/impressorasPesquisarResultado.php
+++ views/impressorasPesquisarResultado.php
@@ -1,7 +1,7 @@
 <?php
 	include "../template/menuPrincipal.php";
     //include "../scripts/mascara.php"; 
-    include "../controllers/impressoraPesquisa.php"; 
+   include_once "../controllers/impressoraPesquisa.php";  
    
     if($_SESSION['impressora'] != "sim")
         {
@@ -9,8 +9,8 @@
             echo "<script>location = '../template/menuPrincipal.php';</script>";  
         }
 
-        //echo "<br>valorPesquisa => " . $valorPesquisa;  
-        echo "<br>tipoPesquisa => " . $tipoPesquisa;  
+    //echo "<br>valorPesquisa => " . $valorPesquisa;  
+    //echo "<br>tipoPesquisa => " . $tipoPesquisa;  
     //$idFuncionario = $_SESSION['idFuncionario'];  
     //$nomeFuncionario = $_SESSION['nomeFuncionario'];  
 ?>	
@@ -59,88 +59,16 @@
                                         </thead>   
                                     </table>  
                                 </div> 
-
-                                <table class="table table-striped table-bordered table-condensed">
-                                    <thead>
-                                        <tr>
-                                            <th>Modelo</th>
-                                            <th>Número Série</th>
-                                            <th>Divisão</th>
-                                            <th>Setor da Máquina</th>
-                                            <th>Localização</th>
-                                            <th>Status</th>
-                                            <th>OPÇÃO</th>
-                                        </tr>
-                                    </thead>  
-                                    <tbody> 
-                                        <?php
-                                            $nomeTabela = "impressoras";								
-                                            require_once '../dao/DAO-controleCir.php';
-                                            $impAltDAO = new ControleCirDAO();
-                                            if($tipoPesquisa != "divisao")
-                                                {
-                                                    foreach($impAltDAO->ListarOpcao2($nomeTabela, $tipoPesquisa, $valorPesquisa) as $imp);
-                                                }
-                                            else
-                                                {
-                                                    foreach($impAltDAO->BuscaPorDivisao($valorPesquisa) as $imp);
-                                                }
-                                            
-                                             
-                                                { ?>													
-                                                    <tr>
-                                                        <td>
-                                                            <?php  
-                                                                $idImpressora = $imp->idImpressora;
-                                                                $modelo1 = $imp->modeloImpressora;
-                                                                $modeloDAO = new ControleCirDAO();
-                                                                foreach($modeloDAO->BuscaModelo($modelo1, $idImpressora) as $resp)
-                                                                    { 	
-                                                                        $modelo = $resp->modeloImpressora;
-                                                                        echo $modelo;
-                                                                    }
-                                                                    
-                                                            ?>
-                                                        </td>
-                                                        <td><?php echo $imp->numSerie ?></td>	
-                                                        <td>
-                                                            <?php   $idSetor = $imp->idSetor; 
-                                                                    $setorDAO = new ControleCirDAO();
-                                                                    foreach($setorDAO->Buscalocal($idSetor) as $Setor)
-                                                                        { 	
-                                                                            $divisao1= $Setor->divisao;
-                                                                            $nomeLocal1 = $Setor->nomeLocal;
-                                                                            $localizacao1 = $Setor->localizacao;
-                                                                            echo $divisao1; 
-                                                                        }
-                                                                    
-                                                            ?>
-                                                        </td>															
-                                                        <td><?php echo $nomeLocal1?></td>	
-                                                        <td><?php echo $localizacao1?></td>		
-                                                        <td><?php echo $imp->statusImpressora?></td>
-                                                        <td class='operations'>
-                                                            <div class="btn-group pull-left" >
-                                                                <a href="./impressoraAlterar.php?action=1&id=<?php echo $imp->idImpressora;?> " class="btn btn-small btn-warning table-edit">A<i class="icon-edit"></i></a>
-                                                            </div>
-                                                            <div class="btn-group pull-left">
-                                                                <a href="./.php?action=1&id=<?php echo $imp->idImpressora; ?>" class="btn btn-primary">H<i class="icon-remove"></i></a>
-                                                            </div>
-                                                            <div class="btn-group pull-left">
-                                                                <a href="./.php?action=1&id=<?php echo $imp->idImpressora; ?>" class="btn btn-info">D<i class="icon-remove"></i></a>
-                                                            </div>
-                                                            <div class="btn-group pull-left">
-                                                                <a href="../.php?action=2&id=<?php echo $imp->idImpressora; ?>" class="btn btn-small btn-danger">I<i class="icon-remove"></i></a>
-                                                            </div>
-                                                            <div class="btn-group pull-left">
-                                                                <a href="../.php?action=2&id=<?php echo $imp->idImpressora; ?>" class="btn btn-small btn-ufs">P<i class="icon-remove"></i></a>
-                                                            </div>
-                                                        </td>			
-                                                    <tr>									
-                                                <?php 
-                                                } ?>							
-                                    </tbody>                                        
-                                </table>  
+                                <?php
+                                   if($tipoPesquisa != "divisao")
+                                        {
+                                            include "../template/exibirPesquisa1.php";  
+                                        }
+                                    else
+                                        {
+                                            include "../template/exibirPesquisa2.php";  
+                                        }     
+                                ?>       		
                             </div>  
                         </div> 						
 					</div>
