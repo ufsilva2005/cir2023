1c7cf51c178fcb8fd5df986d5ed573f32417825e
Ulisses F. da Silva <ufsilva2005@gmail.com>
2023-07-26 16:10:50 -0300

    altercao 15

--- views/impressorasListar.php
+++ views/impressorasListar.php
@@ -20,7 +20,8 @@
 				<hr>
 		
 			<nav class="fixed  navbar navbar-dark">
-                    <div class="col-md-12 py-5 px-2">	 
+				<div class="line col-md-12 p-5 position-absolute top-50 bottom-150 end-150">
+                    <div class="col-md-12 py-5 px-2">	  
 						<table class="table table-striped table-bordered table-condensed">
 							<thead>
 								<tr>
@@ -109,23 +110,23 @@
 							</tbody>								
 						</table>
 					</div>
-				
+				</div>
 			</nav>
 	
 
-		<div class="fixed-bottom p-1 px-md-5 col-md-12 bg-dark text-white">
+		<div class="fixed-bottom p-1 px-md-5 col-md-12 bg-ufs text-black1">
 			<div class="row">
 				<div class="col px-md-1 col-md-2"> 
 					<label for="inputSuccess" class="control-label">Total de Impressoras:</label>
-					<input type="text" class="form-control" value="<?php echo $_SESSION['numUser'];?>" >
+					<input type="text" class="form-control" value="<?php echo $_SESSION['numUser'];?>" readonly >
 				</div>   
 				<div class="col px-md-1 col-md-2"> 
 					<label for="inputSuccess" class="control-label">Impressoras ativas:</label>
-					<input type="text" class="form-control" value="<?php echo $ativo;?>" >
+					<input type="text" class="form-control" value="<?php echo $ativo;?>" readonly>
 				</div>        
 				<div class="col px-md-1 col-md-2"> 
 					<label for="inputSuccess" class="control-label">Impressoras desativadas:</label>
-					<input type="text" class="form-control" value="<?php echo $inativo;?>" >
+					<input type="text" class="form-control" value="<?php echo $inativo;?>" readonly>
 				</div>  
 				<div class="col px-md-1 col-md-6"> 
 					<label for="inputSuccess" class="control-label">Menu Opção:</label>
@@ -152,10 +153,6 @@
 
 											<div class="btn-group pull-left">
 												<a class="btn btn-small btn-ufs">imPrimir<i class="icon-remove"></i></a>
-											</div>
-
-											<div class="btn-group pull-left">
-												<a class='nav-link' href='./pesquisaImpressoras.php'><button type='button' class='btn btn-outline-warning'>voltar</button> </a>
 											</div>
 										</th>
 									</tr>
